77dc3e64521813b246497fc7362a2902afe7ff41
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-12-25 09:46:01 -0600

    Removing unneded prints and changing order of methods

--- multithread.js
+++ multithread.js
@@ -1,8 +1,5 @@
 delayedMethodThatSucceds = () => {
     return new Promise((resolve, reject) => {
-
-        console.log("Starting delayedMethodThatSucceds");
-
         setTimeout(() => {
             try {
                 console.log("Working delayedMethodThatSucceds");
@@ -12,47 +9,22 @@ delayedMethodThatSucceds = () => {
                 reject(error);
             }
         }, 4000)
-
-        console.log("Ending delayedMethodThatSucceds");
     });
 }
 
 delayedMethodThatFails = () => {
     return new Promise((resolve, reject) => {
-
-        console.log("Starting delayedMethodThatFails");
-
         setTimeout(() => {
             try {
                 console.log("Working delayedMethodThatFails");
-                throw "This is an exception on method 2"
+                throw "This is an exception on delayedMethodThatFails"
                 resolve("Success delayedMethodThatFails");
             }
             catch (error) {
                 reject(error);
             }
         }, 2000);
-
-        console.log("Ending delayedMethodThatFails");
     });
-}
-
-asyncMethodHandlingMultiplePromises = async () => {
-    console.log("Starting asyncMethodHandlingMultiplePromises");
-
-    let promises = []
-    promises.push(delayedMethodThatSucceds());
-    promises.push(delayedMethodThatFails());
-
-    // allSettled will wait for each promise to complete 
-    // instead of stopping on the first failure
-    let allResults = await Promise.allSettled(promises);
-
-    allResults.forEach(result => {
-        console.log(`status: ${result.status},  value: ${result.value}, reason: ${result.reason}`);
-    });
-
-    console.log("Ending asyncMethodHandlingMultiplePromises");
 }
 
 methodHandlingOnePromiseOldFashionWay = () => {
@@ -85,7 +57,26 @@ methodHandlingOnePromiseNewWay = async () => {
     console.log("Ending methodHandlingOnePromiseNewWay");
 }
 
-// asyncMethodHandlingMultiplePromises();
-methodHandlingOnePromiseOldFashionWay();
+methodHandlingMultiplePromises = async () => {
+    console.log("Starting methodHandlingMultiplePromises");
+
+    let promises = []
+    promises.push(delayedMethodThatSucceds());
+    promises.push(delayedMethodThatFails());
+
+    // allSettled will wait for each promise to complete 
+    // instead of stopping on the first failure
+    let allResults = await Promise.allSettled(promises);
+
+    allResults.forEach(result => {
+        console.log(`status: ${result.status},  value: ${result.value}, reason: ${result.reason}`);
+    });
+
+    console.log("Ending methodHandlingMultiplePromises");
+}
+
+
+// methodHandlingOnePromiseOldFashionWay();
 // methodHandlingOnePromiseNewWay();
+methodHandlingMultiplePromises();
 
